d7b65c2580d7fab3384b0e9972cbab172bcc36d6
Kai <abcxyz123789654@gmail.com>
2025-09-24 08:52:08 +0700

    Update Footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,27 +1,49 @@
-import { Footer } from "antd-mobile"
+import { Badge, TabBar } from "antd-mobile"
+import {
+  AppOutline,
+  MessageOutline,
+  MessageFill,
+  UnorderedListOutline,
+  UserOutline,
+} from "antd-mobile-icons"
 import "./style.css"
+import { useLocation, useNavigate } from "react-router-dom"
 
 export default function FooterComp() {
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
+
+  const tabs = [
+    {
+      key: "/",
+      title: "Home",
+      icon: <AppOutline />,
+      badge: Badge.dot,
+    },
+    {
+      key: "/history",
+      title: "History",
+      icon: <UnorderedListOutline />,
+      badge: "5",
+    },
+    {
+      key: "/about",
+      title: "About",
+      icon: (active: boolean) => (active ? <MessageFill /> : <MessageOutline />),
+      badge: "99+",
+    },
+    {
+      key: "/profile",
+      title: "Profile",
+      icon: <UserOutline />,
+    },
+  ]
+
   return (
-    <Footer
-      links={[
-        {
-          text: "Home",
-          href: "/",
-        },
-        {
-          text: "History",
-          href: "/history",
-        },
-        {
-          text: "About",
-          href: "/about",
-        },
-        {
-          text: "Profile",
-          href: "/profile",
-        },
-      ]}
-    ></Footer>
+    <TabBar activeKey={pathname} onChange={(value) => navigate(value)}>
+      {tabs.map((item) => (
+        <TabBar.Item key={item.key} icon={item.icon} title={item.title} badge={item.badge} />
+      ))}
+    </TabBar>
   )
 }
